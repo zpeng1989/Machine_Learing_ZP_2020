af0a6c7322b4c5c6deaea4db6bf142ecbd2489e0
unknown <pzhang@aptdc.com>
2021-01-06 17:43:00 +0800

    up cor

--- 特征选择/high_cor_features.py
+++ 特征选择/high_cor_features.py
@@ -7,4 +7,8 @@ dataframe = pd.DataFrame(features)
 
 corr_matrix = dataframe.corr().abs()
 
+upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k = 1).astype(np.bool))
+to_drop = [column for column in upper.columns if any(upper[column] > 0.95)]
+
+print(dataframe.drop(dataframe.columns[to_drop], axis = 1).head(3))
 
